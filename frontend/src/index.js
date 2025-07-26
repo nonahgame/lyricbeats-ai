7a3f5cc8f7035bede2581e157e51f50ffa3cbfcc
nonahgame <103342324+nonahgame@users.noreply.github.com>
2025-07-26 14:16:34 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';  // Added for custom styles
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
